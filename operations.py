539ebba24d35f99d093780ba3329cf31b38f63e0
Julio Briones <julesc00@protonmail.com>
2026-04-27 17:47:10 -0600

    Completed operations.

--- operations.py
+++ operations.py
@@ -21,3 +21,56 @@ def read_task(task_id: int) -> Optional[TaskWithId]:
         for row in reader:
             if int(row["id"]) == task_id:
                 return TaskWithId(**row)
+        return None
+
+
+def get_next_id():
+    try:
+        with open(DATABASE_FILENAME, mode="r") as csvfile:
+            reader = csv.DictReader(csvfile)
+            max_id = max([int(row["id"]) for row in reader])
+            return max_id + 1
+    except (FileNotFoundError, ValueError):
+        return 1
+
+
+def write_task_into_csv(task: TaskWithId) -> None:
+    with open(DATABASE_FILENAME, mode="a") as file:
+        writer = csv.DictWriter(file, fieldnames=column_fields)
+        writer.writerow(task.model_dump())
+
+
+def modify_task(task_id: int, task: dict) -> Optional[TaskWithId]:
+    updated_task: Optional[TaskWithId] = None
+    tasks = read_all_tasks()
+    for idx, task_ in enumerate(tasks):
+        if task_.id == task_id:
+            updated_task = task_.model_copy(update=task)
+            tasks[idx] = updated_task
+
+    with open(DATABASE_FILENAME, mode="w") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=column_fields)
+        writer.writeheader()
+        for t in tasks:
+            writer.writerow(t.model_dump())
+
+    return updated_task
+
+
+def remove_task(task_id: int) -> Optional[Task]:
+    deleted_task: Optional[TaskWithId] = None
+    tasks = read_all_tasks()
+    with open(DATABASE_FILENAME, mode="w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=column_fields)
+        writer.writeheader()
+
+        for t in tasks:
+            if t.id == task_id:
+                deleted_task = t
+                continue
+            writer.writerow(t.model_dump())
+    if deleted_task:
+        dict_task_without_id = deleted_task.model_dump()
+        del dict_task_without_id["id"]
+        return Task(**dict_task_without_id)
+    return None
